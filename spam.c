f5f02e1cc3eba48eb7c8fdd40884e9f71cafa355
MMCH <john.doe@example.com>
2020-11-16 21:15:03 +0100

    printf with beautiful colors + moving instruction in the same condition

--- spam.c
+++ spam.c
@@ -9,6 +9,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define RESET "\x1B[0m"
 
 #define SEND_TRANSACTION "curl -X POST --data '{\"jsonrpc\":\"2.0\",\"method\":\"int_sendTransaction\",\"params\":[{ \
 \"from\": \"INT3HcmxkEx7dDRwVHqEVV46v1dYFsyd\", \
@@ -30,16 +34,16 @@ void delay(int seconds)
 
 void displayMessage(int argv1, int argv2, int argv3)
 {
-	int timer = 5000; // milli seconds
+	int timer = 2000; // milli seconds
 
 	if (argv1 > 1 && argv2 && argv3)
 	{
-		printf("This will send %d TX in total. %d TX every %d seconds... Starting in %d seconds... CTRL + Z for cancel\n", (argv1 * argv2), argv2, (argv3 / 1000), (timer / 1000));
+		printf(YEL "This will send %d TX in total. %d TX every %d seconds... Starting in %d seconds... CTRL + Z for cancel" RESET "\n", (argv1 * argv2), argv2, (argv3 / 1000), (timer / 1000));
 		delay(timer);
 	}
 	else if (argv1 == 1 && argv2 && argv3)
 	{
-		printf("This will send %d TX in total... Starting in %d seconds... CTRL + Z for cancel\n", (argv1 * argv2), (timer / 1000));
+		printf(YEL "This will send %d TX in total... Starting in %d seconds... CTRL + Z for cancel" RESET "\n", (argv1 * argv2), (timer / 1000));
 		delay(timer);
 	}
 
@@ -51,7 +55,7 @@ int	main(int argc, char *argv[])
 	int argv1, argv2, argv3 = 0;
 	if (argc != 4)
 	{
-		printf("usage: %s <number of blocks> <number of tx in a block> <delay (ms) between blocks>\n", argv[0]);
+		printf(RED "usage: %s <number of blocks> <number of tx in a block> <delay (ms) between blocks>\n", argv[0], RESET);
 		return (-1);
 	}
 	else
@@ -61,7 +65,7 @@ int	main(int argc, char *argv[])
 		argv3 = atoi(argv[3]);
 		if (!argv1 || !argv2 || !argv3)
 		{
-			printf("error\n");
+			printf(RED "error\n", RESET);
 			return (-1);
 		}
 		displayMessage(argv1, argv2, argv3);
@@ -70,12 +74,13 @@ int	main(int argc, char *argv[])
 			while(j++ < argv2)
 				system(SEND_TRANSACTION);
 			if (i < argv1)
-				printf("%d TX sent. Next %d TX in %d seconds\n", argv2, argv2, (argv3 / 1000));
-			j = 0;
-			if (i < argv1)
+			{
+				printf(GRN "Success ! %d TX sent. Next %d TX in %d seconds" RESET "\n", argv2, argv2, (argv3 / 1000));
 				delay(argv3); // milli seconds
+			}
+			j = 0;
 		}
-		printf("%d TX sent !\n", (argv1 * argv2));
+		printf(GRN "Success !! %d TX sent !\n", (argv1 * argv2), RESET);
 	}
 	return (0);
 }
